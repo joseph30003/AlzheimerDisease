e53ac8a3442deb5236cfa447fbb8f42981714783
joseph <joseph@172.20.20.20>
2016-07-04 18:53:51 -0400

    add

---
+++ src/database/build.java
@@ -0,0 +1,47 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+//import java.util.ArrayList;
+//import java.util.List;
+
+//import mmserver.MetaMap;
+
+public class build {
+	
+     	
+	
+		public static void main(String[] args)
+	  {
+			
+		
+	
+		try{
+			// create our mysql database connection
+		      
+		      String myUrl = "jdbc:mysql://biomedinformatics.is.umbc.edu/Alzheimer";
+		      
+		      Connection conn = DriverManager.getConnection(myUrl, "weijianqin", "weijianqin");
+		  
+		      NetTable PharmGKB = new PharmGKB(conn);
+		      
+		     PharmGKB.build();
+		    
+		  
+		      
+		    
+		      
+		      conn.close();
+		      }
+		    catch (Exception e)
+		    {
+		      System.err.println("Got an exception! ");
+		      System.err.println(e.getMessage());
+		      e.printStackTrace();
+		    }
+		
+	
+	
+}
+
+}
